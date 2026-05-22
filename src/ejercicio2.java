0b714d3de3d1d03c3d0e59be7a0b6b26a7b3100e
Kevin Ernesto Meza Garcia <erasmomeza11@gmail.com>
2026-05-21 19:56:46 -0600

    Kevin - ejercicio 2 terminado

--- src/ejercicio2.java
+++ src/ejercicio2.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class ejercicio2 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int[] numeros = new int[12];
+        int suma = 0;
+        double promedio;
+
+        // Ingresar números
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Ingrese un número: ");
+            numeros[i] = entrada.nextInt();
+
+            suma += numeros[i];
+        }
+
+        // Calcular promedio
+        promedio = (double) suma / numeros.length;
+
+        // Mostrar resultado
+        System.out.println("\nEl promedio es: " + promedio);
+
+    }
 }
